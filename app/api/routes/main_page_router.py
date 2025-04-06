2408d7fc2f60fb2cdf29330cd80388a3faf0180b
ahnseongeun <ast3138@naver.com>
2025-04-07 00:32:14 +0900

    main page 관련 api 전체 기능 추가

--- app/api/routes/main_page_router.py
+++ app/api/routes/main_page_router.py
@@ -2,7 +2,6 @@
 from typing import List
 
 from fastapi import APIRouter, Depends, Path
-from sqlalchemy.orm import Session
 
 from app.core.deps import get_current_user
 from app.schema.chapter_detail_schema import ChapterDetailResponse, ChapterDetailCreateRequest
@@ -26,7 +25,9 @@ async def get_profile():
 
 # 메인 페이지 조회
 @router.get("/main", response_model=List[ScheduleOverview])
-def get_main_page(uid: str = Depends(get_current_user)):
+def get_main_page(
+        uid: str = Depends(get_current_user)
+):
     return get_main_page_data(uid)
 
 
@@ -34,8 +35,7 @@ def get_main_page(uid: str = Depends(get_current_user)):
 @router.post("/schedule", response_model=ScheduleResponse)
 async def create_schedule_api(
         schedule: ScheduleCreateRequest,
-        uid: str = 'test01'
-        # uid: str = Depends(get_current_user)
+        uid: str = Depends(get_current_user)
 ):
     return create_schedule_for_user(uid, schedule)
 
@@ -43,18 +43,17 @@ async def create_schedule_api(
 # 일정 수정
 @router.put("/schedule/{schedule_id}", response_model=ScheduleResponse)
 def update_schedule_api(
-    schedule_id: str,
-    update_data: ScheduleUpdateRequest,
-    uid: str = 'test01'
-    # uid: str = Depends(get_current_user)
+        schedule_id: str,
+        update_data: ScheduleUpdateRequest,
+        uid: str = Depends(get_current_user)
 ):
     return update_schedule(uid, schedule_id, update_data)
 
+
 @router.delete("/schedule/{schedule_id}")
 def delete_schedule_api(
-    schedule_id: str,
-    uid: str = 'test01'
-    # uid: str = Depends(get_current_user)
+        schedule_id: str,
+        uid: str = Depends(get_current_user)
 ):
     return delete_schedule(uid, schedule_id)
 
@@ -62,10 +61,9 @@ def delete_schedule_api(
 # 챕터 저장
 @router.post("/schedule/{schedule_id}/chapter", response_model=ChapterResponse)
 def create_chapter_api(
-    schedule_id: str = Path(..., description="챕터를 저장할 스케줄 ID"),
-    chapter: ChapterCreateRequest = ...,
-    uid: str = 'test01'
-    # uid: str = Depends(get_current_user)
+        schedule_id: str = Path(..., description="챕터를 저장할 스케줄 ID"),
+        chapter: ChapterCreateRequest = ...,
+        uid: str = Depends(get_current_user)
 ):
     return chapter_service.create_chapter_for_schedule(uid, schedule_id, chapter)
 
@@ -73,11 +71,10 @@ def create_chapter_api(
 # 챕터 수정
 @router.put("/schedule/{schedule_id}/chapter/{chapter_id}", response_model=ChapterResponse)
 async def update_chapter_api(
-    schedule_id: str,
-    chapter_id: str,
-    chapter_data: ChapterUpdateRequest,
-    uid: str = 'test01'
-    # uid: str = Depends(get_current_user)
+        schedule_id: str,
+        chapter_id: str,
+        chapter_data: ChapterUpdateRequest,
+        uid: str = Depends(get_current_user)
 ):
     return update_chapter(uid, schedule_id, chapter_id, chapter_data)
 
@@ -85,12 +82,12 @@ async def update_chapter_api(
 # 챕터 삭제
 @router.delete("/schedule/{schedule_id}/chapter/{chapter_id}")
 def delete_chapter_api(
-    schedule_id: str,
-    chapter_id: str,
-    uid: str = 'test01'
-    # uid: str = Depends(get_current_user)
+        schedule_id: str,
+        chapter_id: str,
+        uid: str = Depends(get_current_user)
 ):
     return delete_chapter(uid, schedule_id, chapter_id)
+
 
 # 챕터 세부내용 생성
 @router.post(
@@ -98,13 +95,13 @@ def delete_chapter_api(
     response_model=ChapterDetailResponse
 )
 def create_chapter_detail_api(
-    schedule_id: str,
-    chapter_id: str,
-    detail: ChapterDetailCreateRequest,
-    uid: str = 'test01'
-    # uid: str = Depends(get_current_user)
+        schedule_id: str,
+        chapter_id: str,
+        detail: ChapterDetailCreateRequest,
+        uid: str = Depends(get_current_user)
 ):
     return create_chapter_detail(uid, schedule_id, chapter_id, detail)
+
 
 # 챕터 세부내용 조회
 @router.get(
@@ -112,11 +109,10 @@ def create_chapter_detail_api(
     response_model=ChapterDetailResponse
 )
 def get_chapter_detail_api(
-    schedule_id: str,
-    chapter_id: str,
-    detail_id: str,
-    uid: str = 'test01',
-    # uid: str = Depends(get_current_user)
+        schedule_id: str,
+        chapter_id: str,
+        detail_id: str,
+        uid: str = Depends(get_current_user)
 ):
     return fetch_chapter_detail(uid, schedule_id, chapter_id, detail_id)
 
@@ -124,10 +120,9 @@ def get_chapter_detail_api(
 # 챕터 세부내용 삭제
 @router.delete("/schedule/{schedule_id}/chapter/{chapter_id}/detail/{detail_id}")
 def delete_chapter_detail_api(
-    schedule_id: str,
-    chapter_id: str,
-    detail_id: str,
-    uid: str = 'test01',
-    # uid: str = Depends(get_current_user)
+        schedule_id: str,
+        chapter_id: str,
+        detail_id: str,
+        uid: str = Depends(get_current_user)
 ):
     return remove_chapter_detail(uid, schedule_id, chapter_id, detail_id)
